352ce3ce7010c3f6a90f8535ea41347b55f349c4
Macintosh <macintosh@Macintoshde-MacBook-Pro.local>
2018-03-12 14:03:44 +0800

    Initial Commit

---
+++ IOSClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSClass
+//
+//  Created by Macintosh on 2018/3/12.
+//  Copyright © 2018年 www. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
